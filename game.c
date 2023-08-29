d3bf218aa0ade26d27624cdff2c3d734ea2397e0
Grisha596 <rijalgrishma5@gmail.com>
2023-08-29 12:36:34 +0545

    Added more code

--- game.c
+++ game.c
@@ -3,3 +3,10 @@
 #include<windows.h>
 #include<stdlib.h>
 #include<string.h>
+
+char square [10]={'o','1','2','3','4','5','6','7','8','9'};
+int checkWin();
+void drawBoard ();
+int menu();
+int start_game();
+void instructions();

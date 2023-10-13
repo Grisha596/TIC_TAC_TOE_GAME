0386b4fad3631ace7bde663d3fcd4a19e3975c7e
Grisha596 <rijalgrishma5@gmail.com>
2023-10-13 14:42:37 +0545

    Adding changes to games

--- game.c
+++ game.c
@@ -109,3 +109,39 @@ printf("==>Game draw<==");
 getch();
 return 0;
 }
+
+int checkWin(){
+if(square[1]==square[2] && square [2]== square[3])
+return 1;
+else if (square[4]== square[5] && square[5]==square[6])
+return 1;
+else if (square[7]== square[8] && square[8]==square[9])
+return 1;
+else if (square[1]== square[4] && square[4]==square[7])
+return 1;
+else if (square[2]== square[5] && square[5]==square[8])
+return 1;
+else if (square[3]== square[6] && square[6]==square[9])
+return 1;
+else if (square[1]== square[5] && square[5]==square[9])
+return 1;
+else if (square[3]== square[5] && square[5]==square[7])
+return 1;
+else if (square[1]!='1' &&square[2]!='2' &&square[3]!='3' &&square[4]!='4' &&square[5]!='5' &&square[6]!='6' &&square[7]!='7' &&square[8]!='8' &&square[9]!='9' )
+return 0;
+else
+return -1;
+}
+
+void drawBoard(){
+system("cls");
+printf("\n\n\t <----TIC TAC TOE GAME---> \n\n");
+printf("player1 (X) -player2 (O)\n\n\n");
+printf("\t\t\t ____ _____ _____\n");
+printf("\t\t\t| %c | %c | %c | \n", square[1],square[2],square[3]);
+printf("\t\t\t|____|_____|_____|\n");
+printf("\t\t\t| %c | %c | %c | \n", square[4],square[5],square[6]);
+printf("\t\t\t|____|_____|_____|\n");
+printf("\t\t\t| %c | %c | %c | \n", square[7],square[8],square[9]);
+printf("\t\t\t|____|_____|_____|\n");
+}
